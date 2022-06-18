f642b455f3eea2de453c1b5fe7ade3fa3c8f51e0
Wildanzr <graita.sukma@gmail.com>
2022-06-18 19:26:36 +0700

    feat: add socmed icons

--- src/components/CardTech.jsx
+++ src/components/CardTech.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const CardTech = ({ logo, title, desc }) => {
   return (
-    <div className="flex flex-col h-full text-black justify-center">
+    <div className="flex flex-col h-full text-[#02091B] justify-center dark:text-[#D8EAF3] duration-300">
       <div className="flex flex-col items-center my-5">
         <img src={logo} alt={title} className="w-8/12 h-auto" />
         <p className="text-2xl font-medium">{title}</p>
