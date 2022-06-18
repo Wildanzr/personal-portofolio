607571e2a2aa89692dbc8589278012262c28b9a5
Wildanzr <graita.sukma@gmail.com>
2022-06-18 12:22:09 +0700

    feat: wheel tech implemented

--- src/components/SwiperCard.jsx
+++ src/components/SwiperCard.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+/* eslint-disable react/prop-types */
+import React from 'react'
 
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { EffectCards } from 'swiper'
@@ -12,77 +13,14 @@ import 'swiper/css/effect-cards'
 // Swiper custom style
 import '../styles/Swiper.css'
 
-import LogoReact from '../images/react.png'
-import LogoNode from '../images/node.png'
-import LogoMongo from '../images/mongo.png'
-import LogoPostgres from '../images/postgres.png'
-import LogoTailwind from '../images/tailwind.png'
-import LogoAws from '../images/aws.png'
-import LogoDocker from '../images/docker.png'
-import LogoGit from '../images/git.png'
-import LogoSpring from '../images/spring.png'
-import LogoLaravel from '../images/laravel.png'
-
-const SwiperCard = () => {
-  const [techs] = useState([
-    {
-      logo: LogoReact,
-      title: 'React',
-      desc: 'React Fundamentals, state, props, hooks, context api, routing, DevTools. Also using Framework such as Ant Design and Material UI.'
-    },
-    {
-      logo: LogoNode,
-      title: 'NodeJS',
-      desc: 'React Fundamentals, state, props, hooks, context api, routing, DevTools. Also using Framework such as Ant Design and Material UI.'
-    },
-    {
-      logo: LogoMongo,
-      title: 'Mongo',
-      desc: 'React Fundamentals, state, props, hooks, context api, routing, DevTools. Also using Framework such as Ant Design and Material UI.'
-    },
-    {
-      logo: LogoPostgres,
-      title: 'Postgres',
-      desc: 'React Fundamentals, state, props, hooks, context api, routing, DevTools. Also using Framework such as Ant Design and Material UI.'
-    },
-    {
-      logo: LogoAws,
-      title: 'AWS',
-      desc: 'React Fundamentals, state, props, hooks, context api, routing, DevTools. Also using Framework such as Ant Design and Material UI.'
-    },
-    {
-      logo: LogoGit,
-      title: 'Git',
-      desc: 'React Fundamentals, state, props, hooks, context api, routing, DevTools. Also using Framework such as Ant Design and Material UI.'
-    },
-    {
-      logo: LogoDocker,
-      title: 'Docker',
-      desc: 'React Fundamentals, state, props, hooks, context api, routing, DevTools. Also using Framework such as Ant Design and Material UI.'
-    },
-    {
-      logo: LogoTailwind,
-      title: 'TailwindCSS',
-      desc: 'React Fundamentals, state, props, hooks, context api, routing, DevTools. Also using Framework such as Ant Design and Material UI.'
-    },
-    {
-      logo: LogoSpring,
-      title: 'Spring Boot',
-      desc: 'React Fundamentals, state, props, hooks, context api, routing, DevTools. Also using Framework such as Ant Design and Material UI.'
-    },
-    {
-      logo: LogoLaravel,
-      title: 'Laravel',
-      desc: 'React Fundamentals, state, props, hooks, context api, routing, DevTools. Also using Framework such as Ant Design and Material UI.'
-    }
-  ])
-
+const SwiperCard = ({ techs }) => {
   return (
-    <div className="flex">
+    <div className="flex mx-20">
       <Swiper
         effect={'cards'}
         grabCursor={true}
         modules={[EffectCards]}
+        initialSlide={5}
         className="mySwiper"
       >
         {techs.map((tech, idx) => {
--- src/views/Service.jsx
+++ src/views/Service.jsx
@@ -1,11 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import CardTech from '../components/SwiperCard'
+import SwiperTech from '../components/SwiperCard'
+import WheelTech from '../components/WheelTech'
+
+import LogoReact from '../images/react.png'
+import LogoNode from '../images/node.png'
+import LogoMongo from '../images/mongo.png'
+import LogoPostgres from '../images/postgres.png'
+import LogoTailwind from '../images/tailwind.png'
+import LogoAws from '../images/aws.png'
+import LogoDocker from '../images/docker.png'
+import LogoGit from '../images/git.png'
+import LogoSpring from '../images/spring.png'
+import LogoLaravel from '../images/laravel.png'
 
 const Service = () => {
+  const [techs] = useState([
+    {
+      logo: LogoReact,
+      title: 'React',
+      desc: 'React Fundamentals, state, props, hooks, context api, routing, DevTools. Also using Framework such as Ant Design and Material UI.'
+    },
+    {
+      logo: LogoNode,
+      title: 'NodeJS',
+      desc: 'React Fundamentals, state, props, hooks, context api, routing, DevTools. Also using Framework such as Ant Design and Material UI.'
+    },
+    {
+      logo: LogoMongo,
+      title: 'Mongo',
+      desc: 'React Fundamentals, state, props, hooks, context api, routing, DevTools. Also using Framework such as Ant Design and Material UI.'
+    },
+    {
+      logo: LogoPostgres,
+      title: 'Postgres',
+      desc: 'React Fundamentals, state, props, hooks, context api, routing, DevTools. Also using Framework such as Ant Design and Material UI.'
+    },
+    {
+      logo: LogoAws,
+      title: 'AWS',
+      desc: 'React Fundamentals, state, props, hooks, context api, routing, DevTools. Also using Framework such as Ant Design and Material UI.'
+    },
+    {
+      logo: LogoGit,
+      title: 'Git',
+      desc: 'React Fundamentals, state, props, hooks, context api, routing, DevTools. Also using Framework such as Ant Design and Material UI.'
+    },
+    {
+      logo: LogoDocker,
+      title: 'Docker',
+      desc: 'React Fundamentals, state, props, hooks, context api, routing, DevTools. Also using Framework such as Ant Design and Material UI.'
+    },
+    {
+      logo: LogoTailwind,
+      title: 'TailwindCSS',
+      desc: 'React Fundamentals, state, props, hooks, context api, routing, DevTools. Also using Framework such as Ant Design and Material UI.'
+    },
+    {
+      logo: LogoSpring,
+      title: 'Spring Boot',
+      desc: 'React Fundamentals, state, props, hooks, context api, routing, DevTools. Also using Framework such as Ant Design and Material UI.'
+    },
+    {
+      logo: LogoLaravel,
+      title: 'Laravel',
+      desc: 'React Fundamentals, state, props, hooks, context api, routing, DevTools. Also using Framework such as Ant Design and Material UI.'
+    }
+  ])
+
   return (
-    <div className="flex flex-col container py-20 px-10">
-        <CardTech />
+    <div className="flex flex-col justify-between container px-10">
+      <h2 className="text-5xl font-medium text-center pb-20">Service</h2>
+
+      <div className="flex flex-row justify-between">
+        <WheelTech techs={techs}/>
+        <SwiperTech techs={techs}/>
+      </div>
     </div>
   )
 }
