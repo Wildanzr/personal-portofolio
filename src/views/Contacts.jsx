166a92623d46c56b2345cd35d2f411d75bfa310b
Wildanzr <graita.sukma@gmail.com>
2022-06-19 11:01:30 +0700

    feat: social media ref

--- src/views/Contacts.jsx
+++ src/views/Contacts.jsx
@@ -4,27 +4,39 @@ import Socials from '../components/Socials'
 
 const Contacts = () => {
   return (
-    <div className="flex flex-col py-20 px-20 justify-center">
+    <div className="flex container flex-col py-20 px-20 justify-center">
       <h2 className="text-5xl font-medium text-center">Contacts</h2>
 
       <div className="flex flex-row justify-around">
         <div className="flex flex-col">
           <div className="flex flex-col">
             <p className="text-xl font-light opacity-60">My Phone Number</p>
-            <span className="text-3xl font-semibold underline py-2">
+            <a
+              href="https://wa.me/+625736822725"
+              rel="noreferrer"
+              target="_blank"
+              className="text-3xl font-semibold underline py-2"
+            >
               +62 85736822725
-            </span>
+            </a>
 
             <p className="text-xl font-light opacity-60 pt-10">My e-mail</p>
-            <span className="text-3xl font-semibold underline py-2">
+            <a
+              href="mailto:graita.sukma@gmail.com"
+              rel="noreferrer"
+              target="_blank"
+              className="text-3xl font-semibold underline py-2"
+            >
               graita.sukma@gmail.com
-            </span>
+            </a>
           </div>
         </div>
 
         <div className="flex flex-col">
           <div className="flex flex-col items-center">
-            <p className="text-xl font-light opacity-60 pb-5">My Social Media</p>
+            <p className="text-xl font-light opacity-60 pb-5">
+              My Social Media
+            </p>
             <Socials />
           </div>
         </div>
